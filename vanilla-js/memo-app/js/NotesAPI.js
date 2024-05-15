989e544f1ea4c93e8966bb0d93a00194c1e40c3f
grara0724 <kazuma.xx24@gmail.com>
2024-05-16 00:49:11 +0900

    バニラjsの練習です

---
+++ vanilla-js/memo-app/js/NotesAPI.js
@@ -0,0 +1,37 @@
+export default class NotesAPI {
+  // すべてのメモを取得するAPI
+  static getAllNotes() {
+    //localStorageにない場合はparseしているので文字列でからの配列を返す
+    const notes = JSON.parse(localStorage.getItem("notes") || "[]");
+    return notes;
+  }
+
+  // メモを保存するAPI
+  static saveNote(noteToSave) {
+    const notes = NotesAPI.getAllNotes();
+    //  =>拡張for文
+    const existingNote = notes.find((note) => note.id == noteToSave.id);
+    // 編集or更新 更新の場合つまりIDが元からある場合はIDは変わらない
+    if (existingNote) {
+      existingNote.title = noteToSave.title;
+      existingNote.body = noteToSave.body;
+      existingNote.updated = new Date().toISOString();
+    } else {
+      // noteToSave.id = Math.floor(Math.random() * 1000000);
+      // noteToSave.updated = new Date().toISOString();
+      // notes.push(noteToSave);
+      noteToSave.id = noteToSave.id;
+      noteToSave.updated = new Date().toISOString();
+      notes.push(noteToSave);
+    }
+    localStorage.setItem("notes", JSON.stringify(notes));
+  }
+  // メモを削除するAPI
+  static deleteNote(id) {
+    const notes = NotesAPI.getAllNotes();
+    // 送られてきたidと一致しない際はnewNotesに保存する
+    const newNotes = notes.filter((note) => note.id !== id);
+
+    localStorage.setItem("notes", JSON.stringify(newNotes));
+  }
+}
---
+++ vanilla-js/memo-app/js/main.js
@@ -0,0 +1,10 @@
+import NotesAPI from "./NotesAPI.js";
+
+// NotesAPI.saveNote({
+//   id: 123456,
+//   title: "二回目のメモです",
+//   body: "プログラミングマスター",
+// });
+
+console.log(NotesAPI.getAllNotes());
+// NotesAPI.deleteNote(327600);
